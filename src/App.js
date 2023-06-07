29a09f4d3c11bb2dde4dcddcd66d4fcbe85e6a2b
陳軒竹LEOCHEN <leochen@mitac.com.tw>
2023-06-07 17:01:41 +0800

    try first

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { useState, useEffect } from 'react';
 import { ReactComponent as AirFlowIcon } from './images/airFlow.svg';
 import { ReactComponent as DayCloudyIcon } from './images/day-cloudy.svg';
 import { ReactComponent as RainIcon } from './images/rain.svg';
 import { ReactComponent as RefreshIcon } from './images/refresh.svg';
 import styled from '@emotion/styled';
+import {ReactComponent as LoadingIcon} from './images/loading.svg'
+
 
 const Container = styled.div`
   background-color: #ededed;
@@ -99,29 +102,157 @@ const Refresh = styled.div`
     width: 15px;
     height: 15px;
     cursor: pointer;
+    animation: rotate infinite 1.5s linear;
+    animation-duration:${({isLoading})=>(isLoading?'1.5s':'0s')};
+  }
+
+  @keyframes rotate{
+    from{
+      transform: rotate(360deg);
+    }
+    to{
+      transform: rotate(0deg);
+    }
   }
 `;
 
 const App = () => {
+  console.log('invoke funciotn component')
+  const AUTHORIZATION_KEY='CWB-65C6C2BE-3323-405E-8C03-1F0BEE3CA28D';
+  const LOCATIONT_NAME='臺北'
+  const LOCATIONT_NAME_FORCAST='臺北市'
+  const [weatherElement, setWeatherElement] = useState({
+    locationName: '',
+    description: '',
+    windSpeed: 0,
+    temperature: 0,
+    rainPosibility: 0,
+    observationTime: new Date(),
+    isLoading: false, 
+    comfortability: '',
+    weatherCode: 0
+  })
+  const {
+    locationName,
+    description,
+    windSpeed,
+    temperature,
+    rainPosibility,
+    observationTime,
+    isLoading,
+    comfortability,
+  }=weatherElement
+  useEffect(()=>{
+    console.log('use effect')
+    fetchCurrentWeather()
+    fetchWeatherForcast()
+  },[])
+  const fetchCurrentWeather = () => {
+    setWeatherElement((prevState) =>({
+      ...prevState,
+      isLoading: true
+    }))
+    fetch(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=${AUTHORIZATION_KEY}&locationName=${LOCATIONT_NAME}`)
+    .then((response)=>response.json())
+    .then((data)=>{
+      const locationData = data.records.location[0];
+      const weatherElements = locationData.weatherElement.reduce(
+        (neededElements, item) => {
+          if (['WDSD','TEMP'].includes(item.elementName))
+            neededElements[item.elementName]=item.elementValue;
+            return neededElements;
+        }
+      )
+      return {
+        observationTime: locationData.time.obsTime,
+        locationName: locationData.locationName,
+        temperature: weatherElements.TEMP,
+        windSpeed: weatherElements.WDSD,
+        isLoading: false
+      }
+      
+      // setWeatherElement((prevState) =>({
+      //   ...prevState,
+      //   observationTime: locationData.time.obsTime,
+      //   locationName: locationData.locationName,
+      //   temperature: weatherElements.TEMP,
+      //   windSpeed: weatherElements.WDSD,
+      //   isLoading: false
+      // }));
+      
+    });
+    
+  }
+
+  const fetchWeatherForcast = () => {
+    // setWeatherElement((prevState) =>({
+    //   ...prevState,
+    //   isLoading: true
+    // }))
+    fetch(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${AUTHORIZATION_KEY}&locationName=${LOCATIONT_NAME_FORCAST}`)
+    .then((response)=>response.json())
+    .then((data)=>{
+      const locationData = data.records.location[0];
+      console.log(data)
+      const weatherElements = locationData.weatherElement.reduce(
+        (neededElements, item) => {
+          if (['Wx','PoP','CI'].includes(item.elementName))
+          {
+            neededElements[item.elementName]=item.time[0].parameter;
+            
+          }
+            
+          return neededElements;
+        },{}
+      )
+      return {
+        description: weatherElements.Wx.parameterName,
+        weatherCode: weatherElements.Wx.parameterValue,
+        rainPosibility: weatherElements.PoP.parameterName,
+        comfortability: weatherElements.CI.parameterName
+      }
+      // setWeatherElement((prevState)=>({
+      //   ...prevState,
+      //   description: weatherElements.Wx.parameterName,
+      //   weatherCode: weatherElements.Wx.parameterValue,
+      //   rainPosibility: weatherElements.PoP.parameterName,
+      //   comfortability: weatherElements.CI.parameterName
+
+      // }));
+    });
+    
+  }
   return (
     <Container>
+      {console.log('render')}
       <WeatherCard>
-        <Location>台北市</Location>
-        <Description>多雲時晴</Description>
+        <Location>{locationName}</Location>
+        <Description>{description} {comfortability}</Description>
         <CurrentWeather>
           <Temperature>
-            23 <Celsius>°C</Celsius>
+          {Math.round(temperature)} <Celsius>°C</Celsius>
           </Temperature>
           <DayCloudy />
         </CurrentWeather>
         <AirFlow>
-          <AirFlowIcon /> 23 m/h
+          <AirFlowIcon /> {windSpeed} m/h
         </AirFlow>
         <Rain>
-          <RainIcon /> 48%
+          <RainIcon /> {rainPosibility} %
         </Rain>
-        <Refresh>
-          最後觀測時間：上午 12:03 <RefreshIcon />
+        <Refresh 
+        onClick={()=>{
+          fetchCurrentWeather();
+          fetchWeatherForcast();
+        }} 
+        isLoading={isLoading}>
+          最後觀測時間：
+          {new Intl.DateTimeFormat('zh-TW',{
+            hour:'numeric',
+            minute: 'numeric'
+          }).format(new Date(observationTime))} 
+          {' '}
+          {isLoading? <LoadingIcon/>: <RefreshIcon />}
         </Refresh>
       </WeatherCard>
     </Container>
